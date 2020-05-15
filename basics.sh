f9163dbba8299edca3140d78cf18e1db99634370
RTS <25514511+realtechsupport@users.noreply.github.com>
2020-05-15 10:55:08 -0400

    Add files via upload

---
+++ basics.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# RTS, feb 2020
+#-------------------------------------------
+clear
+echo "WELCOME - basics for C+R installaton "
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install python3-dev -y
+sudo apt-get install build-essential python3-dev -y
+sudo apt-get install python3-venv -y
+sudo apt-get install ffmpeg -y
+sudo apt-get install sox -y
+
+echo "installed python3, python3dev, python3-venv, ffmpeg "
+
+echo "hit ctrl d to close this session"
+exit 0
+

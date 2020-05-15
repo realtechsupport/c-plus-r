c90d183bb7c9f05c808f5c51bc97eb1766e1e046
RTS <25514511+realtechsupport@users.noreply.github.com>
2020-05-15 14:41:12 -0400

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -12,7 +12,7 @@ sudo apt-get install python3-venv -y
 sudo apt-get install ffmpeg -y
 sudo apt-get install sox -y
 
-echo "installed python3, python3dev, python3-venv, ffmpeg "
+echo "installed python3, python3dev, python3-venv, ffmpeg, sox "
 
 echo "hit ctrl d to close this session"
 exit 0

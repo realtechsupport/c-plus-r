d683034411894c2176b0d6df8199c741c218fdb9
realtechsupport <marcbohlen@gmail.com>
2024-11-22 17:40:16 -0500

    updated requirements 2024

--- basics.sh
+++ basics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # RTS, feb 2020
+# updated nov 2024 (libcurl4)
 #-------------------------------------------
 clear
 echo "WELCOME - basics for C+R installaton "
@@ -11,6 +12,7 @@ sudo apt-get install build-essential python3-dev -y
 sudo apt-get install python3-venv -y
 sudo apt-get install ffmpeg -y
 sudo apt-get install sox -y
+sudo apt-get install libcurl4-openssl-dev
 
 echo "installed python3, python3dev, python3-venv, ffmpeg "
 echo "installed python3, python3dev, python3-venv, ffmpeg, sox "
